8d672f9638042321f94c576b3b1ea5c12b716430
AndersonRR <anderson.ribeiro.r@outlook.com>
2023-09-22 13:58:15 -0300

    feat: adiciona persistência de dados

--- src/store/listStore.ts
+++ src/store/listStore.ts
@@ -1,4 +1,6 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type ItemProps = {
   id: string;
@@ -13,19 +15,28 @@ type ListState = {
   removeItem: (id: string) => void;
 };
 
-const useListStore = create<ListState>()(set => ({
-  items: [],
-  addItem: item => set(state => ({ items: [...state.items, item] })),
-  removeItem: (id: string) => set(state => ({ items: state.items.filter(item => item.id !== id) })),
-  toggleChecked: (id: string) =>
-    set(state => ({
-      items: state.items.map(item => {
-        if (item.id === id) {
-          return { ...item, checked: !item.checked };
-        }
-        return item;
-      }),
-    })),
-}));
+const useListStore = create<ListState>()(
+  persist(
+    set => ({
+      items: [],
+      addItem: item => set(state => ({ items: [...state.items, item] })),
+      removeItem: (id: string) =>
+        set(state => ({ items: state.items.filter(item => item.id !== id) })),
+      toggleChecked: (id: string) =>
+        set(state => ({
+          items: state.items.map(item => {
+            if (item.id === id) {
+              return { ...item, checked: !item.checked };
+            }
+            return item;
+          }),
+        })),
+    }),
+    {
+      name: 'todo-list-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
 
 export { useListStore };
